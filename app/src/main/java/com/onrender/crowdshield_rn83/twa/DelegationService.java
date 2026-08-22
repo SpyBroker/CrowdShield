246902c139345589d49c22ae751c34269a5c2b18
Jahnavi <jahnavi172005@gmail.com>
2026-08-23 02:08:33 +0530

    Add Android project build configs and TWA manifest

---
+++ app/src/main/java/com/onrender/crowdshield_rn83/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.onrender.crowdshield_rn83.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
